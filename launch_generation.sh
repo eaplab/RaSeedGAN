4a03b8ec4447f4532e90e5f22315894b1c9f077f
guemesturb <58606571+guemesturb@users.noreply.github.com>
2022-10-11 10:39:01 +0200

    Update launch_generation.sh

--- launch_generation.sh
+++ launch_generation.sh
@@ -2,7 +2,13 @@ python -u run_generate_tfrecords.py -c channel -u 2 -n 010
 python -u run_generate_tfrecords.py -c channel -u 4 -n 010
 python -u run_generate_tfrecords.py -c channel -u 8 -n 010
 
+python -u run_generate_tfrecords.py -c channel -u 2 -n 010
+python -u run_generate_tfrecords.py -c channel -u 4 -n 010
+python -u run_generate_tfrecords.py -c channel -u 8 -n 010
 
-# python -u run_generate_tfrecords.py -c buoyancy -u 2 -n 015
-# python -u run_generate_tfrecords.py -c buoyancy -u 4 -n 005
-# python -u run_generate_tfrecords.py -c buoyancy -u 8 -n 005
+python -u run_generate_tfrecords.py -c channel -u 2 -n 010
+python -u run_generate_tfrecords.py -c channel -u 4 -n 010
+python -u run_generate_tfrecords.py -c channel -u 8 -n 010
+
+python -u run_generate_tfrecords.py -c exptbl -u 2 -n 0
+python -u run_generate_tfrecords.py -c exptbl -u 4 -n 0
