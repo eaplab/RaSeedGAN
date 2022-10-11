43d90655b6c31fd89935c86fde15ddc0653d493c
guemesturb <58606571+guemesturb@users.noreply.github.com>
2022-10-11 10:43:36 +0200

    Update launch_training.sh

--- launch_training.sh
+++ launch_training.sh
@@ -1,9 +1,18 @@
-python -u run_training.py -c buoyancy -u 2 -m architecture-01-noise-015 -n 15
-# python -u run_training.py -c buoyancy -u 4 -m architecture-01-noise-005 -n 5
-# python -u run_training.py -c buoyancy -u 8 -m architecture-01-noise-005 -n 5
-# python -u run_training.py -c pinball -u 2 -m architecture-01-noise-010 -n 10
-# python -u run_training.py -c pinball -u 4 -m architecture-01-noise-010 -n 10
-# python -u run_training.py -c pinball -u 8 -m architecture-01-noise-010 -n 10
-# python -u run_training.py -c channel -u 2 -m architecture-01-noise-010 -n 10
-# python -u run_training.py -c channel -u 4 -m architecture-01-noise-010 -n 10
-# python -u run_training.py -c channel -u 8 -m architecture-01-noise-010 -n 10
+python -u run_training.py -c pinball -u 2 -m architecture-01-noise-010 -n 10
+python -u run_training.py -c pinball -u 4 -m architecture-01-noise-010 -n 10
+python -u run_training.py -c pinball -u 8 -m architecture-01-noise-010 -n 10
+
+python -u run_training.py -c pinball -u 2 -m architecture-01-noise-050 -n 50
+python -u run_training.py -c pinball -u 4 -m architecture-01-noise-050 -n 50
+python -u run_training.py -c pinball -u 8 -m architecture-01-noise-050 -n 50
+
+python -u run_training.py -c channel -u 2 -m architecture-01-noise-010 -n 10
+python -u run_training.py -c channel -u 4 -m architecture-01-noise-010 -n 10
+python -u run_training.py -c channel -u 8 -m architecture-01-noise-010 -n 10
+
+python -u run_training.py -c sst -u 2 -m architecture-02-noise-000 -n 0
+python -u run_training.py -c sst -u 4 -m architecture-02-noise-000 -n 0
+python -u run_training.py -c sst -u 8 -m architecture-02-noise-000 -n 0
+
+python -u run_training.py -c exptbl -u 2 -m architecture-01-noise-000 -n 0
+python -u run_training.py -c exptbl -u 4 -m architecture-01-noise-000 -n 0
