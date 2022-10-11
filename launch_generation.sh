caf19ea2e380f41f4d5f6c7be694b105e118b05e
guemesturb <58606571+guemesturb@users.noreply.github.com>
2022-10-11 10:40:37 +0200

    Update launch_generation.sh

--- launch_generation.sh
+++ launch_generation.sh
@@ -1,14 +1,18 @@
-python -u run_generate_tfrecords.py -c channel -u 2 -n 010
-python -u run_generate_tfrecords.py -c channel -u 4 -n 010
-python -u run_generate_tfrecords.py -c channel -u 8 -n 010
+python -u run_generate_tfrecords.py -c pinball -u 2 -n 010
+python -u run_generate_tfrecords.py -c pinball -u 4 -n 010
+python -u run_generate_tfrecords.py -c pinball -u 8 -n 010
+
+python -u run_generate_tfrecords.py -c pinball -u 2 -n 050
+python -u run_generate_tfrecords.py -c pinball -u 4 -n 050
+python -u run_generate_tfrecords.py -c pinball -u 8 -n 050
 
 python -u run_generate_tfrecords.py -c channel -u 2 -n 010
 python -u run_generate_tfrecords.py -c channel -u 4 -n 010
 python -u run_generate_tfrecords.py -c channel -u 8 -n 010
 
-python -u run_generate_tfrecords.py -c channel -u 2 -n 010
-python -u run_generate_tfrecords.py -c channel -u 4 -n 010
-python -u run_generate_tfrecords.py -c channel -u 8 -n 010
+python -u run_generate_tfrecords.py -c sst -u 2 -n 0
+python -u run_generate_tfrecords.py -c sst -u 4 -n 0
+python -u run_generate_tfrecords.py -c sst -u 8 -n 0
 
 python -u run_generate_tfrecords.py -c exptbl -u 2 -n 0
 python -u run_generate_tfrecords.py -c exptbl -u 4 -n 0
